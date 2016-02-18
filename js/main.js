e476149bb9e6e5c2769100fbff6c93d01add02a5
Milan Uremovic <milan.uremovic@gmail.com>
2016-02-18 22:53:28 +0100

    add: counter added

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@ $(document).ready(function() {
         });
 
         $('#fd').hide();
+        init_fc();
         next_fc();
 
 
@@ -48,15 +49,28 @@ function show_fc() {
     $('#fd').show();
 }
 
+function init_fc() {
+    q_data['idx'] = 1;
+}
+
 function next_fc() {
 
     var dw = Object.keys(words);
-    var idx = randr(Object.keys(words).length);
+    var len = Object.keys(words).length;
+    var idx = randr(len);
 
-    $('#fdw').html(dw[idx]);
-    $('#fd').html(words[dw[idx]]);
+
+    $('#score').html(q_data['idx']+'/'+len);
+
+    $('#fdw').html(dw[idx-1]);
+    $('#fd').html(words[dw[idx-1]]);
     $('#fd').hide();
 
+    if (len==1) { idx = 1; return 0; }
+
+    delete words[dw[idx]];
+
+    q_data['idx'] += 1;
 }
 
 
