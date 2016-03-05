f0d5e3fe1972aab63c06e0c646379b37179b581c
Milan Uremovic <milan.uremovic@gmail.com>
2016-03-05 21:16:46 +0100

    fixes

--- scripts/create_resjs.sh
+++ scripts/create_resjs.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-cat ~/Dropbox/germanikus/deutsch.md ~/Dropbox/germanikus/deutsch2.md | grep ' = ' | perl doit.pl
+cat ~/Dropbox/germanikus/deutsch.md ~/Dropbox/germanikus/deutsch2.md | grep ' = ' | perl doit.pl > ../js/res.js
 
+cp ~/Dropbox/germanikus/de.html ../
